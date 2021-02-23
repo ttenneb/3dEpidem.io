c2053e78011460e7b066540163a036526c5dcde7
Bennett Garcia <bcg77@scarletmail.rutgers.edu>
2021-02-23 01:40:11 -0500

    --rotation testing

--- public/dist/index.js
+++ public/dist/index.js
@@ -2,15 +2,17 @@ import * as THREE from "../dist/three/build/three.module.js";
 import { FBXLoader } from "../dist/three/examples/jsm/loaders/FBXLoader.js";
 const loader = new FBXLoader();
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 5000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0xEEEEEE);
 renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 const clock = new THREE.Clock();
+//lighting
 scene.add(new THREE.AmbientLight(0x404040, 5));
 const colors = new Uint8Array(200);
+//colors for toon material
 for (let c = 0; c <= colors.length; c++) {
     colors[c] = (c / colors.length) * 256;
 }
@@ -19,11 +21,13 @@ gradientMap.minFilter = THREE.NearestFilter;
 gradientMap.magFilter = THREE.NearestFilter;
 gradientMap.generateMipmaps = false;
 const diffuseColor = new THREE.Color().setHSL(.5, 0.5, .5 * 0.5 + 0.1).multiplyScalar(1 - .5 * 0.2);
+//toon material
 const material = new THREE.MeshToonMaterial({
     color: diffuseColor,
     gradientMap: gradientMap,
     morphTargets: true
 });
+//player module
 let player;
 let mixer;
 loader.load('../dist/res/SK_Character_Dummy_Male_01.fbx', (group) => {
@@ -31,13 +35,13 @@ loader.load('../dist/res/SK_Character_Dummy_Male_01.fbx', (group) => {
         if (object instanceof THREE.Mesh) {
             // @ts-ignore
             //object.material = material;
-            object.receiveShadow = false;
         }
         object.receiveShadow = false;
     });
     group.rotation.y += Math.PI;
     group.rotation.x += Math.PI / 2;
     group.receiveShadow = false;
+    group.scale.set(.5, .5, .5);
     loader.load('../dist/res/walking.fbx', function (object) {
         group.animations = object.animations;
         mixer = new THREE.AnimationMixer(group);
@@ -46,9 +50,8 @@ loader.load('../dist/res/SK_Character_Dummy_Male_01.fbx', (group) => {
     });
     scene.add(group);
     player = group;
-}, (error) => {
-    console.log(error);
 });
+//floor
 const mesh = new THREE.Mesh(new THREE.PlaneGeometry(2000, 2000), new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false }));
 mesh.rotation.z = -Math.PI / 2;
 mesh.receiveShadow = true;
@@ -58,10 +61,13 @@ grid.material.opacity = 0.2;
 grid.rotation.x = -Math.PI / 2;
 grid.material.transparent = true;
 scene.add(grid);
-camera.position.z = 500;
-camera.position.y = -300;
-camera.rotation.x = Math.PI / 4;
+camera.position.z = 1000;
+camera.position.y = -500;
+camera.rotation.x = Math.PI / 6;
+//keyboard_down bools
 let keys = [false, false, false, false];
+let Mouse = new THREE.Vector2(0, 0);
+//keyboard input handler
 function KeyDown(event) {
     if (event.code == "KeyW") {
         keys[0] = true;
@@ -76,6 +82,7 @@ function KeyDown(event) {
         keys[3] = true;
     }
 }
+//keyboard input handler
 function KeyUp(event) {
     if (event.code == "KeyW") {
         keys[0] = false;
@@ -90,32 +97,48 @@ function KeyUp(event) {
         keys[3] = false;
     }
 }
+function MouseMove(event) {
+    Mouse.set(event.x, event.y);
+}
 document.addEventListener('keydown', KeyDown);
 document.addEventListener('keyup', KeyUp);
-const speed = 2;
+document.addEventListener('mousemove', MouseMove);
+const speed = 4;
+let halfPoint = new THREE.Vector2(screen.width / 2, screen.height / 2);
 const animate = function () {
     requestAnimationFrame(animate);
+    //animation clock
     const delta = clock.getDelta();
-    if (mixer && keys[0]) {
+    //animation loop
+    if (mixer && (keys[0] || keys[1] || keys[2] || keys[3])) {
         mixer.update(delta);
         console.log("guhy");
     }
+    let vec = Mouse.sub(halfPoint);
+    vec = vec.normalize();
+    player.rotation.y = vec.angle();
+    //movement from input
     if (keys[0]) {
         camera.position.y += speed;
         player.position.y += speed;
+        player.rotation.y = Math.PI;
     }
     if (keys[1]) {
         camera.position.x += speed;
         player.position.x += speed;
+        player.rotation.y = Math.PI / 2;
     }
     if (keys[2]) {
         camera.position.y -= speed;
         player.position.y -= speed;
+        player.rotation.y = 0;
     }
     if (keys[3]) {
         camera.position.x -= speed;
         player.position.x -= speed;
+        player.rotation.y = 3 * Math.PI / 2;
     }
+    //player rotation
     renderer.render(scene, camera);
 };
 animate();
--- public/src/index.ts
+++ public/src/index.ts
@@ -1,12 +1,13 @@
 import * as THREE from "../dist/three/build/three.module.js"
 import {FBXLoader} from "../dist/three/examples/jsm/loaders/FBXLoader.js";
+import {Vector2} from "three";
 
 
 const loader = new FBXLoader();
 
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 5000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -16,45 +17,50 @@ document.body.appendChild( renderer.domElement );
 
 const clock = new THREE.Clock();
 
+//lighting
 scene.add( new THREE.AmbientLight( 0x404040  ,5) );
 
 const colors = new Uint8Array( 200);
 
+//colors for toon material
 for ( let c = 0; c <= colors.length; c ++ ) {
 
     colors[ c ] = ( c / colors.length ) * 256;
 
 }
+
 const gradientMap = new THREE.DataTexture( colors, colors.length, 1, THREE.LuminanceFormat );
 gradientMap.minFilter = THREE.NearestFilter;
 gradientMap.magFilter = THREE.NearestFilter;
 gradientMap.generateMipmaps = false;
 const diffuseColor = new THREE.Color().setHSL( .5, 0.5, .5 * 0.5 + 0.1 ).multiplyScalar( 1 - .5 * 0.2 );
 
+//toon material
 const material = new THREE.MeshToonMaterial( {
     color: diffuseColor,
     gradientMap: gradientMap,
     morphTargets: true
 } );
 
+//player module
 let player;
 let mixer;
-loader.load(
-    '../dist/res/SK_Character_Dummy_Male_01.fbx',
-    (group) => {
+loader.load('../dist/res/SK_Character_Dummy_Male_01.fbx', (group) => {
 
         group.traverse(function (object) {
             if(object instanceof  THREE.Mesh) {
                 // @ts-ignore
                 //object.material = material;
-                object.receiveShadow=false;
 
             }
             object.receiveShadow=false;
+
         });
        group.rotation.y += Math.PI
        group.rotation.x += Math.PI/2
        group.receiveShadow=false;
+       group.scale.set(.5,.5,.5)
+
 
 
         loader.load( '../dist/res/walking.fbx', function ( object ) {
@@ -67,28 +73,27 @@ loader.load(
 
        scene.add(group);
        player = group;
-    },
-    (error) => {
-        console.log(error);
-    }
-);
+    });
 
-
+//floor
 const mesh = new THREE.Mesh( new THREE.PlaneGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
 mesh.rotation.z = - Math.PI/2;
 mesh.receiveShadow = true;
 scene.add( mesh );
-
 const grid = new THREE.GridHelper( 2000, 20, 0x000000, 0x000000 );
 grid.material.opacity = 0.2;
 grid.rotation.x = - Math.PI/2;
 grid.material.transparent = true;
 scene.add( grid );
 
-camera.position.z = 500;
-camera.position.y = -300;
-camera.rotation.x=Math.PI/4;
+camera.position.z = 1000;
+camera.position.y = -500;
+camera.rotation.x=Math.PI/6;
+
+//keyboard_down bools
 let keys: Array<boolean> = [false, false, false, false];
+let Mouse: Vector2 = new THREE.Vector2(0,0);
+//keyboard input handler
 function KeyDown(event: KeyboardEvent){
     if(event.code == "KeyW"){
         keys[0] = true
@@ -103,6 +108,7 @@ function KeyDown(event: KeyboardEvent){
         keys[3] = true
     }
 }
+//keyboard input handler
 function KeyUp(event: KeyboardEvent){
     if(event.code == "KeyW"){
         keys[0] = false
@@ -117,35 +123,57 @@ function KeyUp(event: KeyboardEvent){
         keys[3] = false
     }
 }
+
+function MouseMove(event:MouseEvent){
+    Mouse.set(event.x,event.y);
+}
+
 document.addEventListener('keydown', KeyDown);
 document.addEventListener('keyup', KeyUp);
+document.addEventListener('mousemove', MouseMove)
 
-const speed = 2;
+const speed = 4;
+let halfPoint =new  THREE.Vector2(screen.width/2,screen.height/2)
 const animate = function () {
     requestAnimationFrame( animate );
-
+    //animation clock
     const delta = clock.getDelta();
-    if ( mixer && keys[0] ){
+    //animation loop
+
+    if ( mixer && (keys[0] || keys[1] || keys[2] || keys[3])){
         mixer.update( delta );
         console.log("guhy")
     }
 
+    let vec = Mouse.sub(halfPoint);
+    vec = vec.normalize();
+    player.rotation.y= vec.angle()
+
+    //movement from input
     if(keys[0]){
         camera.position.y +=speed;
         player.position.y +=speed;
+        player.rotation.y = Math.PI;
     }
     if(keys[1]){
         camera.position.x +=speed;
         player.position.x +=speed;
+        player.rotation.y = Math.PI/2;
     }
     if(keys[2]){
         camera.position.y -=speed;
         player.position.y -=speed;
+        player.rotation.y = 0;
     }
     if(keys[3]){
         camera.position.x -=speed;
         player.position.x -=speed;
+        player.rotation.y = 3*Math.PI/2;
     }
+
+    //player rotation
+
+
     renderer.render( scene, camera );
 };
 
